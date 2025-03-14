d6f857890e595b08d96a4611bd0e7b5d433eb476
pacarti <a.paciorkowski@protonmail.com>
2025-03-14 23:15:32 +0100

    Added fetching transcription to main script + added naming it properly

--- duosynctran.py
+++ duosynctran.py
@@ -1,4 +1,38 @@
-import subprocess, os
+import subprocess, os, requests, bs4
+
+def fetchTranscription(url, outputFileNoExt):
+
+    try:
+        res = requests.get(url)
+    except ConnectionError:
+        print('Main Page doesn\'t exist(or connection is to it is refused)!')
+        exit()
+
+    podcastsAppleSoup = bs4.BeautifulSoup(res.text, 'html.parser')
+
+    pElems = podcastsAppleSoup.select('p')
+
+    linkToTranscription = pElems[1].getText().split()[11].rstrip('.')
+
+
+    try:
+        res_transcription = requests.get(linkToTranscription)
+    except ConnectionError:
+        print('Main Page of transcription doesn\'t exist(or connection is to it is refused)!')
+        exit()
+
+    podcastsTranscriptionSoup = bs4.BeautifulSoup(res_transcription.text, 'html.parser')
+
+    pElemsTranscription = podcastsTranscriptionSoup.select('p')
+
+    transcriptionFile = open(outputFileNoExt + '.txt', 'w')
+
+    for index, pElem in enumerate(pElemsTranscription):
+        if index > 2:
+            transcriptionFile.write(pElem.getText())
+            transcriptionFile.write('\n\n')
+
+    transcriptionFile.close()
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
@@ -7,8 +41,13 @@ url = "https://podcasts.apple.com/us/podcast/le-surfeur-sans-limites-the-surfer-
 # Download the podcast(subprocess, yt-dlp)
 
 
+# result = subprocess.run(["yt-dlp", "--print", "filename", url], capture_output=True, text=True)
 
-result = subprocess.run(["yt-dlp", "--print", "filename", url] ,capture_output=True, text=True)
+result = subprocess.run(["yt-dlp", "--print", "filename", url],
+stdout=subprocess.PIPE,
+stderr=subprocess.PIPE,
+universal_newlines=True) # Ensure output is str, not bytes
+
 
 filename = result.stdout.strip()
 
@@ -16,40 +55,25 @@ print("Downloading the file %s ..." % filename)
 
 subprocess.run("yt-dlp " + url, shell=True)
 
-# print(result)
 
-# print("File " + filename + "downloaded.")
-
-# exit()
-# Remove the cover from mp3 file(subprocess ffmpeg
-# pFilename = 
 outputFileNoExt = filename.split('.mp3')[0]
 
-# print(outputFileNoExt)
 
-# exit()
+# Remove the cover
 
 outputFileNoCover = outputFileNoExt + 'NoCover.mp3'
 
 print('Removing the cover...')
 
-# print(filename)
-# print(outputFileNoCover)
-
-# exit()
 
 command = 'ffmpeg -i \'%s\' -id3v2_version 3 -vn -c:a copy \'%s\'' % (filename, outputFileNoCover)
 print(command)
 
-# exit()
-
-# subprocess.run('ffmpeg -i %s -id3v2_version 3 -vn -c:a copy %s' % (filename, outputFileNoCover), shell=True)
-# subprocess.run('ffmpeg -i '+ filename + ' -id3v2_version 3 -vn -c:a copy ' + outputFileNoCover, shell=True)
 
 subprocess.run(command, shell=True)
 
-# subprocess.run('ffmpeg -i ' + input + ' ' + '-id3v2_version 3 -vn -c:a copy ' + output)
+# Take the transcription from the link(web scraping)
+fetchTranscription(url, outputFileNoExt)
 
-# TODO: Take the transcription from the link(web scraping)
 # TODO: Apply the transcription to the podcast(aeneas)
 # TODO: Apply the output srt to the mp3 file(ffmpeg)
